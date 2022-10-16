ecfea7d69d8a09df4ee87e5a67449a4637e41c5e
alexisrizzo <alexis.rizzo21@gmail.com>
2022-10-16 20:09:29 -0300

    JSX y transpiling

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <img src={"./logo192.png"} className="header__logo" alt="logo" />
+        <div className="header__nav">
+          <ul className='header__nav__menu'>
+            <li>Tienda holistica</li>
+            <li>Deco</li>
+            <li>Regaleria</li>
+          </ul>
+        </div>
+        <div className='header__buttons'>
+          <button>carrito</button>
+        </div>
+        <h1>cinco sentidos</h1>
       </header>
     </div>
   );
