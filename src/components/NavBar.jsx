252a348c00c052acc85b65d19ff90395660cbeb4
alexisrizzo <alexis.rizzo21@gmail.com>
2022-10-20 23:37:03 -0300

    PreEntregaRizzo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,15 +5,15 @@ const NavBar = (props) => {
     return (
         <header className="header">
           <img src={"./logo cs.png"} className="header__logo" alt="logo" />
-        <div className="header__nav">
-          <ul className='header__nav__menu'>
-            <Menu item = "Tienda holistica"/>
-            <Menu item = "Deco"></Menu>
-            <Menu item = "Regaleria"></Menu>
-          </ul>
-        </div>
-        <CartWidget/>
-        <h1>cinco sentidos</h1>
+          <div className="header__nav">
+            <ul className='header__nav__menu'>
+              <Menu item = "Tienda holistica"/>
+              <Menu item = "Deco"></Menu>
+              <Menu item = "Regaleria"></Menu>
+            </ul>
+          </div>
+          <CartWidget/>
+          <h1>cinco sentidos</h1>
       </header>)
 }
 export default NavBar;
